78db4462f12823875edcd7bba36a53eb1b83a8c2
tingting_fang <tingting.ceci@gmail.com>
2022-07-20 18:35:16 -0500

    Fixed paddle boundaries

--- Assets/Scripts/Player2P_2.cs
+++ Assets/Scripts/Player2P_2.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player2P_2 : MonoBehaviour
 {
-    public float speed2 = 0.1f;
+    private float xValue;
+    public float speed2 = 20f;
     public GameObject middlewall;
     public GameObject rightwall;
+    public Transform leftWallTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,27 @@ public class Player2P_2 : MonoBehaviour
         // move with L/R keyboard inputs using Horizontal2 Input
         float xDirection = Input.GetAxis("Horizontal2");
         Vector3 moveDirection = new Vector3(xDirection, 0.0f);
-        transform.position += moveDirection * speed2;
+        //transform.position += moveDirection * speed2 * Time.deltaTime;
+
+        // Keep in the left wall
+        // paddle position + half wall width + half paddle width
+        xValue = (transform.localPosition + moveDirection * speed2 * Time.deltaTime).x + (-1 * leftWallTransform.localScale.x) + (-1 * transform.localScale.x / 2);
+        if (xValue < 0 + 0.5)
+        {
+            Debug.Log("Player 2: out of left boundary");
+        }
+        else if (xValue <= 35 - 3.5 && xValue >= 0 + 0.5)
+        {
+            transform.localPosition += moveDirection * speed2 * Time.deltaTime;
+        }
+        //keep in the right wall
+        else if (xValue > 35 - 3.5)
+        {
+            Debug.Log("Player 2: out of right boundary");
+        }
+        else
+        {
+            transform.localPosition += moveDirection * speed2 * Time.deltaTime;
+        }
     }
 }
